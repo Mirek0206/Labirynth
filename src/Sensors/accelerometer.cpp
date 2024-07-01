93f831189dea35e3ced49989fe527de4198a4853
Damian Mirowski <damian_mirowski@wp.pl>
2024-07-01 09:37:10 +0200

    Added left wall following

--- src/Sensors/accelerometer.cpp
+++ src/Sensors/accelerometer.cpp
@@ -24,25 +24,25 @@ void accelerometerInit( void )
 {
     for ( uint8_t i = 0U; i < INIT_OFFSET_CYCLES; i++ )
     {
-        accelerometerData.x_axis.offset_f32 = ( ( accelerometerData.x_axis.offset_f32 * (float)i ) + ( ( float )analogRead( ACC_X_PIN ) * G_TO_MSS * ACC_SCALE ) ) / (float)( i + 1 );
-        accelerometerData.y_axis.offset_f32 = ( ( accelerometerData.y_axis.offset_f32 * (float)i ) + ( ( float )analogRead( ACC_Y_PIN ) * G_TO_MSS * ACC_SCALE ) ) / (float)( i + 1 );
-        accelerometerData.z_axis.offset_f32 = ( ( accelerometerData.z_axis.offset_f32 * (float)i ) + ( ( float )analogRead( ACC_Z_PIN ) * G_TO_MSS * ACC_SCALE ) ) / (float)( i + 1 );
+        accelerometerData.x_axis.offset_f32 = ( ( accelerometerData.x_axis.offset_f32 * (float)i ) + ( float )analogRead( ACC_X_PIN ) ) / (float)( i + 1 );
+        accelerometerData.y_axis.offset_f32 = ( ( accelerometerData.y_axis.offset_f32 * (float)i ) + ( float )analogRead( ACC_Y_PIN ) ) / (float)( i + 1 );
+        accelerometerData.z_axis.offset_f32 = ( ( accelerometerData.z_axis.offset_f32 * (float)i ) + ( float )analogRead( ACC_Z_PIN ) ) / (float)( i + 1 );
         delay(5U);
     }
     
-    calmanFilterInit(&(accelerometerData.x_axis.calmanData), 0.001F, 0.1F, 0.0F);
-    calmanFilterInit(&(accelerometerData.y_axis.calmanData), 0.001F, 0.1F, 0.0F);
-    calmanFilterInit(&(accelerometerData.z_axis.calmanData), 0.001F, 0.1F, 0.0F);
+    calmanFilterInit(&(accelerometerData.x_axis.calmanData), 0.001F, 0.03F, 0.0F);
+    calmanFilterInit(&(accelerometerData.y_axis.calmanData), 0.001F, 0.03F, 0.0F);
+    calmanFilterInit(&(accelerometerData.z_axis.calmanData), 0.001F, 0.03F, 0.0F);
 };
 
 void accelerometerCycle( void )
 {
-    float raw_axis = ( ( float )analogRead( ACC_X_PIN ) * ACC_SCALE * G_TO_MSS ) - accelerometerData.x_axis.offset_f32;
+    float raw_axis = ( ( float )analogRead( ACC_X_PIN ) - accelerometerData.x_axis.offset_f32 ) * ACC_SCALE * G_TO_MSS;
     accelerometerData.x_axis.value_f32 = calmanFilterUpdate( &(accelerometerData.x_axis.calmanData), raw_axis);
 
-    raw_axis = ( ( float )analogRead( ACC_Y_PIN ) * ACC_SCALE * G_TO_MSS ) - accelerometerData.y_axis.offset_f32;
+    raw_axis = ( ( float )analogRead( ACC_Y_PIN ) - accelerometerData.y_axis.offset_f32 ) * ACC_SCALE * G_TO_MSS;
     accelerometerData.y_axis.value_f32 = calmanFilterUpdate( &(accelerometerData.y_axis.calmanData), raw_axis);
 
-    raw_axis = ( ( float )analogRead( ACC_Z_PIN ) * ACC_SCALE * G_TO_MSS ) - accelerometerData.z_axis.offset_f32;
+    raw_axis = ( ( float )analogRead( ACC_Z_PIN ) - accelerometerData.z_axis.offset_f32 ) * ACC_SCALE * G_TO_MSS;
     accelerometerData.z_axis.value_f32 = calmanFilterUpdate( &(accelerometerData.z_axis.calmanData), raw_axis);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define MOTOR_SHIELD_M2_DIR 10U
 #define MOTOR_SHIELD_M2_PWM 9U
 
-#define MOTOR_DEFAULT_SPEED 200U
+#define MOTOR_DEFAULT_SPEED 100
 
 DRV8835MotorShield motors( MOTOR_SHIELD_M1_DIR, 
                            MOTOR_SHIELD_M1_PWM, 
@@ -19,6 +19,7 @@ DRV8835MotorShield motors( MOTOR_SHIELD_M1_DIR,
                            MOTOR_SHIELD_M2_PWM );
 
 void driveForward( void );
+void followLeftWall ( void );
 
 void setup() 
 {
@@ -37,29 +38,30 @@ void loop()
   accelerometerCycle();
   ultrasonicSensorCycle();
 
-  // Serial.print( "Left: " );
-  // Serial.print( ultrasonicSensorData.left_f32 );
-  // Serial.print( " cm | Front: ");
-  // Serial.print( ultrasonicSensorData.front_f32 );
-  // Serial.print( " cm | Right: " );
-  // Serial.print( ultrasonicSensorData.right_f32 );
-  // Serial.print( " cm | " );
-  Serial.print( accelerometerData.x_axis.value_f32 );
-  Serial.print( " | " );
-  Serial.print( accelerometerData.y_axis.value_f32 );
-  Serial.print( " | " );
-  Serial.print( accelerometerData.z_axis.value_f32 );
-  Serial.print( " | " );
-  Serial.print( accelerometerData.x_axis.offset_f32 );
-  Serial.print( " | " );
-  Serial.print( accelerometerData.y_axis.offset_f32 );
-  Serial.print( " | " );
-  Serial.print( accelerometerData.z_axis.offset_f32 );
-  Serial.print( " | " );
+  Serial.print( "Left: " );
+  Serial.print( ultrasonicSensorData.left_f32 );
+  Serial.print( " cm | Front: ");
+  Serial.print( ultrasonicSensorData.front_f32 );
+  Serial.print( " cm | Right: " );
+  Serial.print( ultrasonicSensorData.right_f32 );
+  Serial.print( " cm | " );
+  // Serial.print( accelerometerData.x_axis.value_f32 );
+  // Serial.print( " | " );
+  // Serial.print( accelerometerData.y_axis.value_f32 );
+  // Serial.print( " | " );
+  // Serial.print( accelerometerData.z_axis.value_f32 );
+  // Serial.print( " | " );
+  // Serial.print( accelerometerData.x_axis.offset_f32 );
+  // Serial.print( " | " );
+  // Serial.print( accelerometerData.y_axis.offset_f32 );
+  // Serial.print( " | " );
+  // Serial.print( accelerometerData.z_axis.offset_f32 );
+  // Serial.print( " | " );
 
 
 
-  driveForward();
+  //driveForward();
+  followLeftWall();
 }
 
 void driveForward( void )
@@ -67,14 +69,59 @@ void driveForward( void )
   int16_t leftMotorSpeed_s16 = MOTOR_DEFAULT_SPEED;
   int16_t rightMotorSpeed_s16 = MOTOR_DEFAULT_SPEED;
 
-
-  if ( accelerometerData.y_axis.value_f32 > 0.06F )
+  if ( ultrasonicSensorData.front_f32 < 8.0 )
   {
-    leftMotorSpeed_s16 += 15;
+    leftMotorSpeed_s16 = 0;
+    rightMotorSpeed_s16 = 0;
+  }
+  else if ( accelerometerData.y_axis.value_f32 > 0.06F )
+  {
+    leftMotorSpeed_s16 += 25;
   }
   else if ( accelerometerData.y_axis.value_f32 < -0.06F )
   {
-    rightMotorSpeed_s16 += 15;
+    rightMotorSpeed_s16 += 25;
+  }
+
+  Serial.print( " | Speed: " );
+  Serial.print( leftMotorSpeed_s16 );
+  Serial.print( " | " );
+  Serial.println( rightMotorSpeed_s16 );
+
+  motors.setM1Speed( rightMotorSpeed_s16 ); // prawy
+  motors.setM2Speed( leftMotorSpeed_s16 ); // lewy
+}
+
+void followLeftWall ( void )
+{
+  int16_t leftMotorSpeed_s16 = MOTOR_DEFAULT_SPEED;
+  int16_t rightMotorSpeed_s16 = MOTOR_DEFAULT_SPEED;
+  static uint8_t wallDetected = 0U;
+  static float distance = 0.0F;
+  const float threshold = 0.5F;
+
+  if (   ultrasonicSensorData.front_f32 < 15.0F
+      || ultrasonicSensorData.left_f32 > 50.0F )
+  {
+    leftMotorSpeed_s16 &= 0x00;
+    rightMotorSpeed_s16 &= 0x00;
+    wallDetected &= 0x00;
+  }
+  else if ( 1U == wallDetected )
+  {
+    if ( ( ultrasonicSensorData.left_f32 - distance ) > threshold )
+    {
+      leftMotorSpeed_s16 -= 25;
+    }
+    else if ( ( distance - ultrasonicSensorData.left_f32 ) > threshold )
+    {
+      rightMotorSpeed_s16 -= 25;
+    }
+  }
+  else
+  {
+    distance = ultrasonicSensorData.left_f32;
+    wallDetected |= 0x01;
   }
 
   Serial.print( " | Speed: " );
